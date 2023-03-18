61a5dba2d1733ae8df52faf7a1d029b08255bc80
Benjamin Steinig <bsteinig@umich.edu>
2023-03-18 14:50:39 -0400

    Added .pt to gitignore.

--- GenreClassifier.py
+++ GenreClassifier.py
@@ -1,73 +1,33 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class GenreClassifier(nn.Module):
-    def __init__(self, classes=5):
+    def __init__(self):
         super(GenreClassifier, self).__init__()
-
-        self.conv1 = nn.Conv1d(10, 8, kernel_size=3, stride=1)
-        self.bn1 = nn.BatchNorm1d(8)
-        self.relu1 = nn.ReLU()
-        self.pool1 = nn.AvgPool1d(kernel_size=2)
-
-        self.conv2 = nn.Conv1d(8, 16, kernel_size=3, stride=1)
-        self.bn2 = nn.BatchNorm1d(16)
-        self.relu2 = nn.ReLU()
-        self.pool2 = nn.AvgPool1d(kernel_size=2)
-
-        self.conv3 = nn.Conv1d(16, 32, kernel_size=3, stride=1)
-        self.bn3 = nn.BatchNorm1d(32)
-        self.relu3 = nn.ReLU()
-        self.pool3 = nn.AvgPool1d(kernel_size=2)
-
-        self.conv4 = nn.Conv1d(32, 64, kernel_size=3, stride=1)
-        self.bn4 = nn.BatchNorm1d(64)
-        self.relu4 = nn.ReLU()
-        self.pool4 = nn.AvgPool1d(kernel_size=2)
-
-        self.conv5 = nn.Conv1d(64, 128, kernel_size=3, stride=1)
-        self.bn5 = nn.BatchNorm1d(128)
-        self.relu5 = nn.ReLU()
-        self.pool5 = nn.AvgPool1d(kernel_size=2)
-
-        self.flatten = nn.Flatten()
-
-        self.dropout = nn.Dropout(p=0.3)
-
-        self.fc = nn.Linear(2816, classes)
-        self.softmax = nn.Softmax(dim=1)
+        self.conv1 = nn.Conv1d(10, 32, kernel_size=3)
+        self.bn1 = nn.BatchNorm1d(32)
+        self.conv2 = nn.Conv1d(32, 64, kernel_size=3)
+        self.bn2 = nn.BatchNorm1d(64)
+        self.conv3 = nn.Conv1d(64, 128, kernel_size=3)
+        self.bn3 = nn.BatchNorm1d(128)
+        self.conv4 = nn.Conv1d(128, 256, kernel_size=3)
+        self.bn4 = nn.BatchNorm1d(256)
+        self.pool = nn.AvgPool1d(kernel_size=3, stride=2)
+        self.fc1 = nn.Linear(256 * 36, 512)
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(512, 5)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu1(x)
-        x = self.pool1(x)
-
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu2(x)
-        x = self.pool2(x)
-
-        x = self.conv3(x)
-        x = self.bn3(x)
-        x = self.relu3(x)
-        x = self.pool3(x)
-
-        x = self.conv4(x)
-        x = self.bn4(x)
-        x = self.relu4(x)
-        x = self.pool4(x)
-
-        x = self.conv5(x)
-        x = self.bn5(x)
-        x = self.relu5(x)
-        x = self.pool5(x)
-
-        x = self.flatten(x)
-
-        x = self.dropout(x)
-
-        x = self.fc(x)
-        x = self.softmax(x)
-
-        return x
+        x = F.relu(self.bn1(self.conv1(x)))
+        print(x.shape)
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        print(x.shape)
+        x = F.relu(self.bn3(self.conv3(x)))
+        print(x.shape)
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
+        print(x.shape)
+        x = x.view(-1, 256 * 36)
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.fc2(x)
+        return F.softmax(x, dim=1)
